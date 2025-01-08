bffd28b42cedf38dc7ffc24b2f4e714f6accae01
Javi0708 <99759216+Javi0708@users.noreply.github.com>
2025-01-08 09:49:57 +0100

    Add files via upload

---
+++ Primer trimestre/Ejercicio 21.py
@@ -0,0 +1,9 @@
+import math
+nombre=input(print("Nombre de usuario:"))
+print("bienvenido",nombre,"al programa 1")
+texto=input(print("Introduce el texto:"))
+long=len(texto)
+print("El texto ocupa",long,"caracteres")
+radio=float(input(print("Introduce el radio:")))
+area=math.pi*radio**2
+print("El area del circulo es",area)
---
+++ Primer trimestre/Ejercicio 22.py
@@ -0,0 +1,10 @@
+text ="La lluvia en Sevilla es una maravilla"
+print(text)
+if "lluvia" in text:
+  print("El texto contiene'lluvia'")
+
+if "pura" in text:
+  print("El texto contiene'pura'")
+
+if "villa" in text:
+  print("El texto contiene'villa'")
