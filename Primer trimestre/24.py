ccfb84cca49ea75d13cff5be6ee46566d744bcd7
Javi0708 <99759216+Javi0708@users.noreply.github.com>
2025-01-19 20:05:33 +0100

    Add files via upload

--- Primer trimestre/24.py
+++ Primer trimestre/24.py
@@ -1,20 +1,12 @@
+
 frase = input("Introduce una frase: ")
+
 letra = input("Introduce una letra: ")
 
-if len(letra) != 1:
-    print("Debes introducir solo una letra.")
-else:
-    contador_while = 0
-    i = 0
-    while i < len(frase):
-        if frase[i] == letra:
-            contador_while += 1
-        i += 1
+contador = 0
 
-    contador_for = 0
-    for caracter in frase:
-        if caracter == letra:
-            contador_for += 1
+for caracter in frase:
+    if caracter == letra:
+        contador += 1
 
-    print(f"Con while, la letra '{letra}' aparece {contador_while} veces.")
-    print(f"Con for, la letra '{letra}' aparece {contador_for} veces.")
+print(f"La letra '{letra}' aparece {contador} veces en la frase.")
